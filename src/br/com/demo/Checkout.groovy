e79cd2ec4d31cb8d795eacb92ad0044bd98e22fc
Stefany Cristina Oliveira De Souza <T804950@tokiomarine.com.br>
2020-02-17 13:29:36 -0300

    Adjusting params

--- src/br/com/demo/Checkout.groovy
+++ src/br/com/demo/Checkout.groovy
@@ -1,14 +1,15 @@
 package br.com.demo
+
 class Checkout {
 
 	private String _url
 	private String _credentialId
 	private String _branchName
 
-	Checkout (String url, String credentialId, String branchName) {
-		_url = url
-		_credentialId = credentialId
-		_branchName = branchName
+	Checkout (Map params) {
+		_url = params.url
+		_credentialId = params.credentialId
+		_branchName = params.branchName
 	}
 
 	void execute() {
--- vars/Checkout.groovy
+++ vars/Checkout.groovy
@@ -2,7 +2,7 @@ import br.com.demo.Checkout
 
 def call(Map params) {
     
-    def checkout = new Checkout(params.url, params.credentialId, params.branchName)
+    def checkout = new Checkout(params)
     checkout.execute()
     
 }
