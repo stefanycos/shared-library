e1ba48d43bc481463c3211f54bbeb58b24e2a156
Stefany Cristina Oliveira De Souza <T804950@tokiomarine.com.br>
2020-02-17 13:24:14 -0300

    Adjusting pameters

--- vars/Checkout.groovy
+++ vars/Checkout.groovy
@@ -1,8 +1,8 @@
 import br.com.demo.Checkout
 
-def call(String url, String credentialId, String branchName) {
+def call(Map params) {
     
-    def checkout = new Checkout(url, credentialId, branchName)
+    def checkout = new Checkout(params.url, params.credentialId, params.branchName)
     checkout.execute()
     
 }
